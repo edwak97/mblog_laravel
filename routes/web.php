677a71fcc9dd4428bb9c6645c18bd07d4ba99d2e
Eduard Kilba <>
2026-03-02 19:35:32 +0300

    [NC] Some broken containerization

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,8 @@ use App\Http\Controllers\NoteController;
 
 Route::resource('notes', NoteController::class)->only('index');
 
+//Route::get('/', function () {
+//   return 'Hello World';
+//});
+
 Route::redirect('/', '/notes', 302);
