37830baf5157d408c9a5f2a0b021c58d2316228f
Eduard Kilba <>
2026-03-10 00:15:19 +0300

    Style update

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>edwak97</title>
 	<link rel="stylesheet" href="{{asset('base.css')}}">
+	<link rel="icon" type="image/x-icon" href="{{asset('favicon.ico')}}">
 </head>
 
 <body>
